542f4dd2812780340cdd4ab6cd325ef9e835795b
Ed <zhrebec_edvard@mail.ru>
2023-04-07 07:53:54 +0300

    Добавлено env

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -58,7 +58,7 @@ const login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch(next);
